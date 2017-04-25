f3f6a45b034db3dd54d33c0b9e062a00cab703af
Josh Abraham <sinisterpatrician@gmail.com>
2017-04-25 18:10:57 -0400

    PEP8 fix on chapter2 exercises

--- chapter1/ex2.py
+++ chapter1/ex2.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 num_one = int(input("Please enter your first number: "))
-# Entering a decimal number here results in a invalid literal 
+# Entering a decimal number here results in a invalid literal
 # for int() with base 10 error
 # This is because python is expecting an int, not a float
 num_two = int(input("Please enter your second number: "))
-# Entering a non-numeric value here results in a invalid literal 
+# Entering a non-numeric value here results in a invalid literal
 # for int() with base 10 error
 
 product = num_one * num_two
