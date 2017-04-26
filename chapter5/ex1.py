104cf8ef6e3027dae876b930a53f278db71d5aac
Joshua Abraham <sinisterpatrician@gmail.com>
2017-04-26 16:02:26 -0400

    Add first ex from chapter 5

---
+++ chapter5/ex1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def pos_elements(num_list):
+    pos_list = []
+    for num in num_list:
+        if num > 0:
+            pos_list.append(num)
+    return pos_list
+
+test_list = range(-3, 10)
+
+print(pos_elements(test_list))
