874c42c0c4bcb132f3cd5f01061dca7881f165ed
Josh Abraham <sinisterpatrician@gmail.com>
2017-04-28 09:58:18 -0400

    Add project writeup and small fixes to string formatting in project

--- projects/jabraham_bottles.py
+++ projects/jabraham_bottles.py
@@ -18,15 +18,17 @@ Take one down
 And pass it around
 {2} bottle{3} of beer on the wall!\n'''
 
+    plural_s = "s"
+
     if bottle_number == 2:
-        return master_verse.format(2, 's', 1, '')
+        return master_verse.format(2, plural_s, 1, '')
     if bottle_number == 1:
         last_verse = master_verse.split("\n")
         last_verse[-2] = "No more bottles of beer on the wall!\n"
         last_verse = "\n".join(last_verse)
         return last_verse.format(1, '')
 
-    return master_verse.format(bottle_number, 's', bottle_number - 1, 's')
+    return master_verse.format(bottle_number, plural_s, bottle_number - 1, plural_s)
 
 
 def main():
