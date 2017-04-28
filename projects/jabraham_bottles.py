a8f7fc9ea5dc8b3bca671c0749c15798bf89bdfa
Josh Abraham <sinisterpatrician@gmail.com>
2017-04-28 08:33:11 -0400

    Add command line parameter

--- projects/jabraham_bottles.py
+++ projects/jabraham_bottles.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python3
+'''
+This program prints the lyrics to the song "99 Bottles"
+'''
+import sys
 
 
 def generate_verse(bottle_number):
-    ''' Return a string of verse from the song "99 Bottles" corresponding to bottle_number'''
+    '''
+    Return a string of verse from the song "99 Bottles"
+    corresponding to bottle_number
+    '''
+
     master_verse = '''
 {0} bottle{1} of beer on the wall!
 {0} bottle{1} of beer!
 Take one down
 And pass it around
 {2} bottle{3} of beer on the wall!\n'''
-    
+
     if bottle_number == 2:
         return master_verse.format(2, 's', 1, '')
     if bottle_number == 1:
-        last_verse =  master_verse.split("\n")
+        last_verse = master_verse.split("\n")
         last_verse[-2] = "No more bottles of beer on the wall!\n"
         last_verse = "\n".join(last_verse)
         return last_verse.format(1, '')
@@ -22,8 +30,22 @@ And pass it around
 
 
 def main():
-    # TODO: add command-line arg
-    for num in range(99, 0, -1):
+    '''
+    Recieves one command line argument and prints the appropriate
+    lyrics to the song "99 Bottles. Default value is 99"
+    '''
+
+    if len(sys.argv) == 2:
+        user_number = sys.argv[1]
+        if user_number.isdigit() and user_number != "0":
+            user_number = int(user_number)
+        else:
+            print("Usage: {} <number of bottles (must be more than 0)>".format(sys.argv[0]))
+            exit()
+    else:
+        user_number = 99
+
+    for num in range(user_number, 0, -1):
         print(generate_verse(num))
 
 
