5b08bdb0c24fef8c07dc2150dbcdc740329be3bd
Josh Abraham <sinisterpatrician@gmail.com>
2017-05-03 11:10:57 -0400

    Add ex3 chapter 8

---
+++ chapter8/ex3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+''' Code for exercise 3 chapter 8 '''
+
+def main():
+    ''' Docstring '''
+
+    prompt = "Please enter an integer: "
+    error = "\nPlease exit with ctrl+d on Linux or ctrl+z on Windows"
+    total = 0
+
+    while True:
+        try:
+            user_input = input(prompt)
+            user_input = int(user_input)
+            total += user_input
+        except (ValueError, IndexError) as e:
+            print("Error:", e)
+        except KeyboardInterrupt:
+            print(error)
+        except EOFError:
+            break
+    
+    print("\nThe total is {}".format(total))
+
+if __name__ == "__main__":
+    main()
