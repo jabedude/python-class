079a5b57a5b09b5d1f450fb6bf1c989f5cb85ce0
Josh Abraham <sinisterpatrician@gmail.com>
2017-05-03 15:31:49 -0400

    Add ex6 chapter 9

---
+++ chapter9/ex6.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+''' Code for exercise 6 chapter 9 '''
+import sys, os
+
+
+def main():
+    '''
+    Displays file name, size, and modification date for files in
+    directory bigger than given size
+    '''
+    dir_name = sys.argv[1]
+    min_size = int(sys.argv[2])
+    file_dict = dict()
+    msg = '''Filename: {}
+Size: {}
+Modification Date: {}'''
+
+    dir_list = os.listdir(dir_name)
+    for dir_item in dir_list:
+        file_stat = os.stat(dir_name + "/" + dir_item)
+        if file_stat.st_size > min_size:
+            file_dict[dir_item] = file_stat
+
+    for item in file_dict.items():
+        print(msg.format(item[0], item[1][6], item[1][8]))
+
+if __name__ == "__main__":
+    main()
