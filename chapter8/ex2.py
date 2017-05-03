dbe861b7c0cc81e28635dd177d7e13cc52aa9620
Josh Abraham <sinisterpatrician@gmail.com>
2017-05-03 10:59:55 -0400

    Add ex2 from Chapter 8

---
+++ chapter8/ex2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+''' Code for exercise 2 chapter 8 '''
+
+def main():
+    ''' Docstring '''
+
+    number_list = range(1, 11)
+    prompt = "Please enter a number (end to quit): "
+
+    while True:
+        user_input = input(prompt)
+        if user_input == "end":
+            exit()
+        try:
+            user_input = int(user_input)
+
+            if user_input < 0:
+                raise IndexError("number must be positive")
+
+            print(number_list[user_input])
+        except (ValueError, IndexError) as e:
+            print("Error:", e)
+
+if __name__ == "__main__":
+    main()
