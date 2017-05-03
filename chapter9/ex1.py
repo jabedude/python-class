f193a28e4b53d709c010b4c656a7b6ed823dfece
Josh Abraham <sinisterpatrician@gmail.com>
2017-05-03 14:24:17 -0400

    Add ex1 and 2 for chapter 9

---
+++ chapter9/ex1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+''' Code for exercise 1 chapter 9 '''
+
+def main():
+    ''' Docstring '''
+    line_count = 0
+    word_count = 0
+    char_count = 0
+    file_name = input("Please enter a file name: ")
+
+    with open(file_name, "r") as count_file:
+        while True:
+            line = count_file.readline()
+            if not line:
+                break
+            line_count += 1
+            word_count += len(line.split())
+            char_count += len(line)
+
+    msg = "{} {} {} {}".format(line_count, word_count, char_count, file_name)
+    print(msg)
+
+if __name__ == "__main__":
+    main()
---
+++ chapter9/ex2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+''' Code for exercise 1 chapter 9 '''
+import sys
+
+def main():
+    ''' Docstring '''
+    if len(sys.argv) == 3 and sys.argv[1] == "-t":
+        file_name = sys.argv[2]
+    else:
+        file_name = input("Please enter a file name: ")
+
+    line_count = 0
+    word_count = 0
+    char_count = 0
+
+    with open(file_name, "r") as count_file:
+        while True:
+            line = count_file.readline()
+            if not line:
+                break
+            line_count += 1
+            word_count += len(line.split())
+            char_count += len(line)
+
+    msg = "{} {} {} {}".format(line_count, word_count, char_count, file_name)
+    print(msg)
+
+if __name__ == "__main__":
+    main()
